6306d32aaebbf0b75301997a5c388966c49ea8b2
AlumnoMañana <AlumnoMaana@DESKTOP-KK9UC82>
2024-02-28 13:47:37 +0100

    hola

---
+++ src/main/java/com/mycompany/tarea4_3_1netbeans/Tarea4_3_1NetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea4_3_1netbeans;
+
+/**
+ *
+ * @author AlumnoMañana
+ */
+public class Tarea4_3_1NetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
